ff80ec00a369807bc9f4a461b082465cf8ee4830
gabicristina <gaabi.cristina@gmail.com>
2014-07-06 23:18:43 -0300

    Alteração biblioteca
    Alteração biblioteca

--- www/js/document_web.js
+++ www/js/document_web.js
@@ -3,6 +3,7 @@ var doc;
 Vex.Flow.Test.Web = {};
 var i = 0;
 var windowScoreTop = 0;
+var measuresInBlock = 10000;
 
 Vex.Flow.Test.Web.Start = function() {
 	module("Document");
@@ -31,6 +32,24 @@ Vex.Flow.Test.Web.xmlSimple = function(options, contextBuilder) {
 	
 	doc.getFormatter().setWidth(1200).drawBlockMeasure(5, 1, ctx);
 	ok(true, "drew document");
+	
+	var ourMeasureNum = 4; // indexed from 0
+	var canvasNum = 0;
+	for (var i = 0; i < formatter.measuresInBlock.length; i++)
+	    for (var j = 0; i < formatter.measuresInBlock[i].length; j++)
+	        if (formatter.measuresInBlock[i][j] == ourMeasureNum) break;
+	var canvas = formatter.canvases[canvasNum];
+	var context = canvas.getContext('2d');
+	var measureStaves = formatter.vfStaves[ourMeasureNum];
+	var x = measureStaves[0].x + measureStaves[0].width/2;
+	var y0 = measureStaves[0].y
+	var lastStaff = measureStaves[measureStaves.length-1];
+	var y1 = lastStaff.y + lastStaff.height;
+
+	context.beginPath();
+	context.moveTo(x, y0);
+	context.moveTo(x, y1);
+	context.stroke();
 };
 
 function errorReaderHandler(evt) {
@@ -65,44 +84,10 @@ Vex.Flow.Test.Web.xmlDoc = function(options, contextBuilder) {
 	//formatter.setWidth(800);
 	var ctx = new contextBuilder(options.canvas_sel, 750, 130);
 	ctx.scale(1.2, 1.2);
-	docWeb.getFormatter().setWidth(600).drawBlock(i, ctx);
+	var docFormatter = docWeb.getFormatter().setWidth(600);
+	docFormatter.drawBlock(i, ctx);
+	measuresInBlock = docFormatter.measuresInBlock.length;
+	alert(measuresInBlock);
 	//formatter.drawBlock(0, ctx);
 	ok(true, "drew document");
 };
-
-//rAF
-window.requestAnimationFrame = function() {
-	return window.requestAnimationFrame ||
-		window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame ||
-		window.msRequestAnimationFrame ||
-		window.oRequestAnimationFrame ||
-		function(f) {
-			window.setTimeout(f,1e3/60);
-		}
-}();
-
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-
-var W = canvas.width;
-var H = canvas.height;
-
-// We want to move/slide/scroll the background
-// as the player moves or the game progresses
-
-// Velocity X
-var vx = 0;
-
-(function renderGame() {
-	window.requestAnimationFrame(renderGame);
-	
-	ctx.clearRect(0, 0, W, H);
-	
-	ctx.fillStyle = '#333';
-	ctx.fillRect(0, 0, 500, 400);
-	
-	Vex.Flow.Test.runTest("MusicXML Document Test", Vex.Flow.Test.Web.xmlDoc);
-	
-	vx -= 2;
-}());
